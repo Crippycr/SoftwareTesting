94a66e3adeccfa2a882ee38cd77a62742d7cdc16
Crippycr <112234802+Crippycr@users.noreply.github.com>
2023-12-06 21:23:48 -0800

    Update UserInterfaceTests.cs

--- src/PlaywrightTests/UserInterfaceTests.cs
+++ src/PlaywrightTests/UserInterfaceTests.cs
@@ -21,23 +21,23 @@ public class UserInterfaceTests
 
         IPage page = await context.NewPageAsync();
         //Navigate to Google.com
-        await page.GotoAsync("https://www.google.com");
+        await page.GotoAsync("https://letsusedata.com/index.html");
         IReadOnlyList<IFrame> f = page.Frames;
         if (f.Count > 1)
         {
             await f[1].ClickAsync("text=No thanks");
         }
         // Search Playwright
-        await page.FillAsync("[aria-label=\"Search\"]", "Playwright");
+        await page.FillAsync("[id=txtUser]", "Test1");
+        await page.FillAsync("[id=txtPassword]", "12345678");
         // Press Enter
-        var response = await page.RunAndWaitForNavigationAsync(async () => await page.PressAsync("[aria-label=\"Search\"]", "Enter"));
         //Click on the first search option
-        await page.ClickAsync("xpath=//h3[contains(text(),'Playwright: Fast and reliable end-to-end testing')]");
+        await page.ClickAsync("id=javascriptLogin");
         //Verify Page URL
-        Assert.Equal("https://playwright.dev/", page.Url);
+        //Assert.Equal("https://playwright.dev/", page.Url);
         // Click text=Get started
-        await page.ClickAsync("text=Get Started");
+        //await page.ClickAsync("text=Get Started");
         //Verify Page URL
-        Assert.Equal("https://playwright.dev/docs/intro", page.Url);
+        //Assert.Equal("https://playwright.dev/docs/intro", page.Url);
     }
 }
